7965b46d89529b36546c1c795be5f3fc2cbf2fe3
Amal Nasr <nasr.amal88@gmail.com>
2024-08-01 11:58:41 +0200

    Deployed website

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Assurez-vous que le répertoire de sortie est correct
+  },
 })
