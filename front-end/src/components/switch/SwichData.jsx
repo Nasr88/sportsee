97774acdbee4c204f329cb0322b10d3ccfa971ed
Amal Nasr <nasr.amal88@gmail.com>
2024-08-16 13:26:48 +0200

    fix title

--- front-end/src/components/switch/SwichData.jsx
+++ front-end/src/components/switch/SwichData.jsx
@@ -6,13 +6,13 @@ import { DataContext } from '../../contexts/DataContext';
 
 function SwitchData(){
     const { mocked, setMocked } = useContext(DataContext); 
-    
+
     const toggleSwitch = () => {
         setMocked(prevData => !prevData);
     }
     return (
         <div className='switch'>
-            <span className="switch__txt">Données mockes ?</span>
+            <span className="switch__txt">Données mockées ?</span>
             <label className="switch__btn">
                 <input type="checkbox" checked={mocked} onChange={toggleSwitch} />
                 <span className="slider"></span>
